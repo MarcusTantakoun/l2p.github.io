de2ac7275da1e4a3e6ebc58595d16b9f3a19d6f2
Marcus Tantakoun <marcustantakoun@Marcuss-MacBook-Pro.local>
2025-05-29 10:06:27 -0400

    updated docs

---
+++ docs/conf.py
@@ -0,0 +1,63 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "L2P"
+copyright = "2024, Marcus Tantakoun"
+author = "Marcus Tantakoun"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+    "sphinx_copybutton",
+]
+
+myst_enable_extensions = [
+    "colon_fence",  # Enables Markdown for code block with language
+]
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_title = "l2p"
+html_static_path = ["_static"]
---
+++ docs/l2p.rst
@@ -0,0 +1,63 @@
+L2P
+================
+Below are the in-depth usage of L2P. It is **highly** recommended to use the base template found in :doc:`templates` to properly extract LLM output into the designated Python formats from these methods.
+
+PromptBuilder
+-------------
+.. autoclass:: l2p.PromptBuilder
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+DomainBuilder
+-------------
+.. autoclass:: l2p.DomainBuilder
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+TaskBuilder
+-----------
+.. autoclass:: l2p.TaskBuilder
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+FeedbackBuilder
+---------------
+.. autoclass:: l2p.FeedbackBuilder
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+Utils
+-----
+The `utils` package contains several helper modules for working with PDDL and L2P processes.
+
+PDDL Parser
+~~~~~~~~~~~
+.. automodule:: l2p.utils.pddl_parser
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+PDDL Planner
+~~~~~~~~~~~~
+.. automodule:: l2p.utils.pddl_planner
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+PDDL Types
+~~~~~~~~~~
+.. automodule:: l2p.utils.pddl_types
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+PDDL Validator
+~~~~~~~~~~~~~~
+.. automodule:: l2p.utils.pddl_validator
+   :members:
+   :undoc-members:
+   :inherited-members:
